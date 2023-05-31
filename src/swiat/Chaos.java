8f96e47bc4089fd92bd19e03e65cf9a1dad758eb
Rollczi <ndejlich5@gmail.com>
2023-05-31 22:23:18 +0200

    Add time to Chaos.

--- src/swiat/Chaos.java
+++ src/swiat/Chaos.java
@@ -1,11 +1,16 @@
 package swiat;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Chaos {
 
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
     private final Swiat swiat = new Swiat();
 
     private void naPoczatkuBylo(Slowo slowo) {
-        System.out.println("Na początku było " + slowo);
+        LocalTime time = LocalTime.now();
+        System.out.println("Na początku było " + slowo + " o godzinie " + time.format(formatter));
 
         this.swiat.wlacz();
     }
